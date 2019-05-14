73c28e0d51d7d806b0e0a718bbac2eb3cfa88d67
Daniel Cavalcante <c0500272@vale.com>
2019-05-14 17:20:22 -0300

    Count

--- module_1/5_app/src/App.js
+++ module_1/5_app/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Hello from './components/Hello'
 import Props from './components/Props'
 import DefaultProps from './components/DefaultProps'
+import Count from './components/Count'
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <Hello />
       <Props nomeEmpresa="Softcom" />
       <DefaultProps nomeEmpresa="Softcom" />
+      <Count />
     </div>
   );
 }
