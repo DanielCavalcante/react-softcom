255870b7821224d19e84a2ba0fd82fc62cc5e16e
Daniel Cavalcante <c0500272@vale.com>
2019-05-14 16:45:44 -0300

    Add Hello and Props component

--- module_1/5_app/src/App.js
+++ module_1/5_app/src/App.js
@@ -2,23 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Hello from './components/Hello'
+import Props from './components/Props'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello />
+      <Props nomeEmpresa="Softcom" />
     </div>
   );
 }
