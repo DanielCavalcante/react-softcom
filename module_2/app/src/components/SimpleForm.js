4f0f79fb9d56894404a33de6db5867b4dccf555f
Daniel Cavalcante <c0500272@vale.com>
2019-05-20 00:40:06 -0300

    Add checkbox

--- module_2/app/src/components/SimpleForm.js
+++ module_2/app/src/components/SimpleForm.js
@@ -5,7 +5,8 @@ export default class SimpleForm extends Component {
   constructor() {
     super()
     this.state = {
-      value: 'Default value'
+      value: 'Default value',
+      checked: false
     }
   }
 
@@ -15,6 +16,9 @@ export default class SimpleForm extends Component {
         <input type='text' value={this.state.value} onChange={(e) => {
           this.setState({value: e.target.value})
         }} />
+        <br />
+        <input type='checkbox' value='OK' checked={this.state.checked}
+          onChange={(e) => {this.setState({ checked: e.target.checked })}} /> Checkbox
       </form>
     )
   }
