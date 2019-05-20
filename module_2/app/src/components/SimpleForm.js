c360a6457937c247db3d07a65cc38800bbd25348
Daniel Cavalcante <c0500272@vale.com>
2019-05-20 00:55:28 -0300

    Add select

--- module_2/app/src/components/SimpleForm.js
+++ module_2/app/src/components/SimpleForm.js
@@ -22,6 +22,14 @@ export default class SimpleForm extends Component {
         <br />
         <input type='radio' name='radio' value='1' defaultChecked />1 
         <input type='radio' name='radio' value='2' />2
+        <br />
+
+        <select value='3'>
+          <option value='1'>Opção 1</option>
+          <option value='2'>Opção 2</option>
+          <option value='3'>Opção 3</option>
+        </select>
+
       </form>
     )
   }
