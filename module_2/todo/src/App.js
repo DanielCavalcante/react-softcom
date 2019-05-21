ab86739581e39bc6bcdb700b2db6789584dccb82
Daniel Cavalcante <c0500272@vale.com>
2019-05-21 20:40:55 -0300

    Add fontawsome and fix buttons

--- module_2/todo/src/App.js
+++ module_2/todo/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Routes from './components/Routes'
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css'
 
 function App() {
   return (
